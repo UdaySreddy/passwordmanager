d627d8baa16e7a6797bffaa815b69154558d347e
UdaySreddy <103050184+UdaySreddy@users.noreply.github.com>
2022-05-31 18:14:26 +0530

    Update index.js

--- src/ListElements/index.js
+++ src/ListElements/index.js
@@ -5,16 +5,33 @@ import './index.css'
 class ListElements extends Component {
   render() {
     const {listGen} = this.props
+    const {webSite, userName} = listGen
+    const First = webSite.slice(0, 1)
 
     return (
-      <ul>
-        {listGen.map(each => (
-          <li key={each.id}>
-            <p>{each.webSite}</p>
-            <p>{each.userName}</p>
-          </li>
-        ))}
-      </ul>
+      <li className="listElement">
+        <div className="div1">
+          <h1 className="letter">{First}</h1>
+        </div>
+        <div className="div1">
+          <p>{webSite}</p>
+          <p>{userName}</p>
+          <img
+            className="imageee"
+            src="https://assets.ccbp.in/frontend/react-js/password-manager-stars-img.png"
+            alt="stars"
+          />
+        </div>
+        <div className="div1">
+          <button className="buttonn" type="button">
+            <img
+              className="icon"
+              src="https://assets.ccbp.in/frontend/react-js/password-manager-delete-img.png"
+              alt="delete"
+            />
+          </button>
+        </div>
+      </li>
     )
   }
 }
